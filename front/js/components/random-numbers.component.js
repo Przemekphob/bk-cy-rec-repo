eaca49a0d57531a03b6b73e58e443e83640cbb1a
Przemek Mazur <Przemek1713@gmail.com>
2018-06-01 14:53:54 +0200

    random numbers component change, promise added with return

--- front/js/components/random-numbers.component.js
+++ front/js/components/random-numbers.component.js
@@ -1,7 +1,6 @@
 function RandomNumbers(selector) {
     Component.call(this, selector);
     this.numbers = [];
-    this.allNumbers = [];
 }
 
 RandomNumbers.prototype = Object.create(Component.prototype);
@@ -9,35 +8,35 @@ RandomNumbers.constructor = RandomNumbers;
 
 RandomNumbers.prototype.init = function() {
     const self = this;
-    
-    axios.get('http://localhost:3000/random-numbers')
-    .then(function(response) {
-        self.numbers = response.data.data.map(function(number) {
-            self.allNumbers.push(number);
-            //console.log(self.allNumbers);
+}
 
-            return {
-                id: number
-            }
-        });
-        self.render();
+RandomNumbers.prototype.fetchNewNumbers = function() {
+    const self = this;
+
+    let promise = axios.get('http://localhost:3000/random-numbers')
+    .then(function(response) {
+        self.numbers = response.data.data;
+
+        console.log('got new numbers: ' + self.numbers);
     })
     .catch(function(error) {
         console.error(error);
     });
-    
 
+    return promise;
 };
 
 RandomNumbers.prototype.render = function() {
     const container = this.getDOMElement();
+
     while(container.firstChild) {
         container.removeChild(container.firstChild);
     }
+
     this.numbers.forEach(function(number) {
         const listElement = document.createElement('li');
         listElement.classList.add('list-group-item');
-        listElement.innerHTML = number.id;
+        listElement.innerHTML = number;
 
         container.appendChild(listElement);
     });
